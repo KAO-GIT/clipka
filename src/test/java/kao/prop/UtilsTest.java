f348836d99a450b3b1e4343230be1b2b6bbe2342
KAO-GIT <kao_z@bk.ru>
2022-09-17 20:20:27 +0500

    ...

--- src/test/java/kao/prop/UtilsTest.java
+++ src/test/java/kao/prop/UtilsTest.java
@@ -1,6 +1,5 @@
 package kao.prop;
 
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.awt.event.KeyEvent;
@@ -25,7 +24,7 @@ class UtilsTest
 		final String s = "&";
 		int keys[] = new int[]{KeyEvent.VK_ALT};
 		assertTrue(s=="&");
-		assertArrayEquals(keys, keys);
+		//assertArrayEquals(keys, keys);
 //		Utils.pressWithComposeKeys(keys, s);
 
 //		Runnable r = () ->  
@@ -58,47 +57,47 @@ class UtilsTest
 
 	}
 	
-	@Test
-	void testPress() throws Exception
-	{
-		//final Robot robot = KeyUtil.getRobot(); 
-		final java.awt.Robot robot = new java.awt.Robot();
-		final String s = "{alt}{numpad1}{numpad2}{numpad9}";
-		KeyStructs kss = (KeyUtil.getKeyStructs(s)).get(0); 
-		//int[] f = new int[] {KeyEvent.VK_ALT} ; // 
-		int[] f = KeyUtil.getKeysForRobot(kss.get(0));
-		pressKeys(f,robot);
-		
-		//robot.keyPress(KeyEvent.VK_ALT); 
-		for (int i = 1; i < kss.size(); i++)
-		{
-			var codes = KeyUtil.getKeysForRobot(kss.get(i)); 
-			//Utils.pressReleaseKeys(,1,false,robot);
-			
-			System.out.println(""+codes[0]);
-			
-			IntStream.range(0, codes.length).forEachOrdered(j ->
-			{
-				robot.keyPress(codes[j]);
-				robot.delay(1);
-				robot.keyRelease(codes[j]);
-			});
-			
-		}
-		//robot.keyRelease(KeyEvent.VK_ALT);
-		releaseKeys(f,robot);
-		
-		// @@@@@@@1129
-	}
-	
-	private static void pressKeys(int[] keys, java.awt.Robot robot)
-	{
-		IntStream.range(0, keys.length).forEachOrdered(i -> robot.keyPress(keys[i]));
-	}
-
-	private static void releaseKeys(int[] keys, java.awt.Robot robot)
-	{
-		IntStream.iterate(keys.length - 1, i -> i >= 0, i -> i = i - 1).forEachOrdered(i -> robot.keyRelease(keys[i]));
-	}
+//	@Test
+//	void testPress() throws Exception
+//	{
+//		//final Robot robot = KeyUtil.getRobot(); 
+//		final java.awt.Robot robot = new java.awt.Robot();
+//		final String s = "{alt}{numpad1}{numpad2}{numpad9}";
+//		KeyStructs kss = (KeyUtil.getKeyStructs(s)).get(0); 
+//		//int[] f = new int[] {KeyEvent.VK_ALT} ; // 
+//		int[] f = KeyUtil.getKeysForRobot(kss.get(0));
+//		pressKeys(f,robot);
+//		
+//		//robot.keyPress(KeyEvent.VK_ALT); 
+//		for (int i = 1; i < kss.size(); i++)
+//		{
+//			var codes = KeyUtil.getKeysForRobot(kss.get(i)); 
+//			//Utils.pressReleaseKeys(,1,false,robot);
+//			
+//			System.out.println(""+codes[0]);
+//			
+//			IntStream.range(0, codes.length).forEachOrdered(j ->
+//			{
+//				robot.keyPress(codes[j]);
+//				robot.delay(1);
+//				robot.keyRelease(codes[j]);
+//			});
+//			
+//		}
+//		//robot.keyRelease(KeyEvent.VK_ALT);
+//		releaseKeys(f,robot);
+//		
+//		// @@@@@@@1129
+//	}
+//	
+//	private static void pressKeys(int[] keys, java.awt.Robot robot)
+//	{
+//		IntStream.range(0, keys.length).forEachOrdered(i -> robot.keyPress(keys[i]));
+//	}
+//
+//	private static void releaseKeys(int[] keys, java.awt.Robot robot)
+//	{
+//		IntStream.iterate(keys.length - 1, i -> i >= 0, i -> i = i - 1).forEachOrdered(i -> robot.keyRelease(keys[i]));
+//	}
 	
 }
